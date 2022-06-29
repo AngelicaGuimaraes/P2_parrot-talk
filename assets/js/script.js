92f433ff4ca86b4639cd443d7f47a64f4b5f3ac6
AngelicaGuimaraes <angelicaguimaraes76@gmail.com>
2022-06-29 10:37:21 +0000

    Add quiz-feedback container content

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-console.log("Connected!");
+// define variables to select elements
+
+const startButton = document.getElementById("start");
+const finishButton = document.getElementById("finish");
+const restartButton = document.getElementById("restart");
+const helloText = document.getElementById("hello");
+const questionAnswerContainer = document.getElementById("question-answer-container");
+const questionText = document.getElementById("question-text");
+const optionsButtons = document.getElementById("options-buttons");
+const answerInfo = document.getElementById("answer-info");
+const infoText = document.getElementById("info");
