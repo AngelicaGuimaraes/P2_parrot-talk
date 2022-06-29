312e4af1ee813978c298a6526cc5084f7865d185
AngelicaGuimaraes <angelicaguimaraes76@gmail.com>
2022-06-29 11:18:30 +0000

    Add variables to run quiz and event listeners

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// define variables to select elements
+    // define variables to select elements
 
 const startButton = document.getElementById("start");
 const finishButton = document.getElementById("finish");
@@ -13,3 +13,21 @@ const quizFeedback = document.getElementById("quiz-feedback");
 const feedbackText = document.getElementById("feedback-text");
 const scoreFeedback = document.getElementById("score-feedback");
 const restartText = document.getElementById("restart-text");
+
+    // define variables to drive quiz
+
+/**
+ *  provides an index number for the current question, which is then incremented until all questions have been used
+ */
+let currentQuestionIndex;
+
+/**
+ * this variable is used to randomise the order of the questions found in the array of questions
+ * and will create a new array in a different order each time it is used let shuffledQuestions
+ */
+ let shuffledQuestions;
+
+    // event listeners
+
+startButton.addEventListener('click', startQuiz);
+restartButton.addEventListener('click', startQuiz);
